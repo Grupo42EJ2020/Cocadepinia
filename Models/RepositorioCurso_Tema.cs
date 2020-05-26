820644a6ece0149b4f1fe40a947f7380ae1a1928
Armando Enrique Mascorro Garcia <armandomascorrogarcia@gmail.com>
2020-05-26 17:20:54 -0500

    Agregacion de clases Curso_Tema

---
+++ Models/RepositorioCurso_Tema.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCLaboratorio.Models
+{
+    public class RepositorioCurso_Tema : ICurso_Tema
+    {
+        public List<Curso_Tema> obtenerCurso_Temas()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Empleado obtenerCurso_Tema(int IdCT)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void insertarCurso_Tema(Curso_Tema datosCurso_Tema)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void eliminarCurso_Tema(int IdCT)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void actualizarCurso_Tema(Curso_Tema datosCurso_Tema)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
